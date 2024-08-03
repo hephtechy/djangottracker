9c9d40576711160d7afc4bed87e5ded80717ba68
unknown <hephtech01@gmail.com>
2024-08-03 06:33:10 +0100

    troubleshooting

--- authentications/settings.py
+++ authentications/settings.py
@@ -14,10 +14,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = 'RENDER' not in os.environ
+DEBUG = True
+# DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['127.0.0.1','.ngrok.io']
+
+ALLOWED_HOSTS = ['127.0.0.1', 'https://ttracker-django.onrender.com', '.ngrok.io']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
